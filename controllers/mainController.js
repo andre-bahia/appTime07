c36deda07937b6229be79e7beb6adc131aa0b32c
davidcorbetta <davidcorbetta@hotmail.com>
2017-03-30 22:26:09 -0300

    teste

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,5 +1,9 @@
 app.controller("mainController", mainController);
 
-function mainController () {
+function mainController ($scope) {
     
+	$scope.Nome = "Time07";
+
+	$scope.inputText = {};
+	console.log($scope.Nome);
 }
